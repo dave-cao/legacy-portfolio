fb799cd5c3d131344113cf4f2e9a3b77826ae57f
david cao <david@dcao@ualberta.ca>
2021-09-04 14:25:10 -0600

    finished making the personal portfolio going mobile first. Next on the list is to expand to desktop sizes!

--- main2.js
+++ main2.js
@@ -1,1 +1,8 @@
 console.log("hello world")
+
+const SUBMIT_BUTTON = document.querySelector("#submitButton");
+console.log(SUBMIT_BUTTON);
+
+const SUBMIT_PROMPT = () => {
+    alert("Thank you for viewing my page! Unfortunately, this form function has not been implemented yet. To contact me, please email dcao@ualberta.ca. Thank you!")
+}
