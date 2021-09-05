b2d2023739f3fc600dbec282015452ebb508bc0f
david cao <david@dcao@ualberta.ca>
2021-09-04 23:25:00 -0600

    playing around with javascript and forms

--- main2.js
+++ main2.js
@@ -1,8 +1,28 @@
-console.log("hello world")
 
 const SUBMIT_BUTTON = document.querySelector("#submitButton");
-console.log(SUBMIT_BUTTON);
+const user_name = document.getElementById("user_name");
+const user_email = document.getElementById("user_email")
+const user_occupation = document.getElementById("user_occupation")
+const user_comments = document.getElementById("user_comments")
 
 const SUBMIT_PROMPT = () => {
-    alert("Thank you for viewing my page! Unfortunately, this form function has not been implemented yet. To contact me, please email dcao@ualberta.ca. Thank you!")
+    alert("Thank you for viewing my page! Unfortunately, this form function has not been implemented yet. To contact me, please email dcao@ualberta.ca. Thank you!");
 }
+
+const TESTING = (form) => {
+    let TestVar = form.user_name.value;
+    alert ("You typed: " + TestVar);
+}
+
+// const writeText = (form) => {
+//     user_name.innerHTML = form.user_name.value;
+//     user_email.innerHTML = form.user_email.value;
+//     user_occupation.innerHTML = form.user_occupation.value;
+//     user_comments.innerHTML = form.user_comments.value;
+
+// }
+
+
+// var form = document.getElementById("myForm");
+// function handleForm(event) { event.preventDefault(); } 
+// form.addEventListener('submit', handleForm);
